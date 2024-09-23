7b41aa13515b61337bd9b9e2f56576dfa34c8a42
Rebecca Harbury <rebecca.harbury@metoffice.gov.uk>
2024-09-23 13:34:52 +0100

    Broken weather display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,48 +17,57 @@ import { combineLatest } from 'rxjs';
 
 export class AppComponent implements OnInit{
   headers: HttpHeaders;
-  hourlyData:any = [];
-  dailyData:any = [];
-  allData:any = [];
-  location_data:any = [];
+  hourlyData;
+  dailyData;
 //  searchTerm= '';
-  place_name;
+//  place_name;
   place_data = [
-    {name: 'Okehampton', lat: 50.7390, lon: -4.0032}, 
+    {name: 'Okehampton', lat: 50.7390, lon: -4.0032},
     {name: 'Torbay', lat: 50.4517, lon: -3.5579}, 
     {name: 'Woodbury', lat: 50.6768, lon: -3.4005}];
   constructor(public httpClient: HttpClient) {
     let headers = new HttpHeaders();
-    this.headers = headers.set('apikey', 'eyJ4NXQiOiJOak16WWpreVlUZGlZVGM0TUdSalpEaGtaV1psWWpjME5UTXhORFV4TlRZM1ptRTRZV1JrWWc9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJyZWJlY2NhLmhhcmJ1cnlAbWV0b2ZmaWNlLmdvdi51a0BjYXJib24uc3VwZXIiLCJhcHBsaWNhdGlvbiI6eyJvd25lciI6InJlYmVjY2EuaGFyYnVyeUBtZXRvZmZpY2UuZ292LnVrIiwidGllclF1b3RhVHlwZSI6bnVsbCwidGllciI6IlVubGltaXRlZCIsIm5hbWUiOiJzaXRlX3NwZWNpZmljLWI4ZmM2NTUxLWU2MGEtNGI1My04Mzk5LTY1NTVkODczMjdhZiIsImlkIjo2MTI4LCJ1dWlkIjoiMDRjMTE0MWYtZDA2YS00NDM1LWI0YWMtMWE5MTY2YjdmN2YxIn0sImlzcyI6Imh0dHBzOlwvXC9hcGktbWFuYWdlci5hcGktbWFuYWdlbWVudC5tZXRvZmZpY2UuY2xvdWQ6NDQzXC9vYXV0aDJcL3Rva2VuIiwidGllckluZm8iOnsid2RoX3NpdGVfc3BlY2lmaWNfZnJlZSI6eyJ0aWVyUXVvdGFUeXBlIjoicmVxdWVzdENvdW50IiwiZ3JhcGhRTE1heENvbXBsZXhpdHkiOjAsImdyYXBoUUxNYXhEZXB0aCI6MCwic3RvcE9uUXVvdGFSZWFjaCI6dHJ1ZSwic3Bpa2VBcnJlc3RMaW1pdCI6MCwic3Bpa2VBcnJlc3RVbml0Ijoic2VjIn19LCJrZXl0eXBlIjoiUFJPRFVDVElPTiIsInN1YnNjcmliZWRBUElzIjpbeyJzdWJzY3JpYmVyVGVuYW50RG9tYWluIjoiY2FyYm9uLnN1cGVyIiwibmFtZSI6IlNpdGVTcGVjaWZpY0ZvcmVjYXN0IiwiY29udGV4dCI6Ilwvc2l0ZXNwZWNpZmljXC92MCIsInB1Ymxpc2hlciI6IkphZ3Vhcl9DSSIsInZlcnNpb24iOiJ2MCIsInN1YnNjcmlwdGlvblRpZXIiOiJ3ZGhfc2l0ZV9zcGVjaWZpY19mcmVlIn1dLCJ0b2tlbl90eXBlIjoiYXBpS2V5IiwiaWF0IjoxNzI1NDQ4NTM1LCJqdGkiOiI5MzgwYmViMS1mZGE1LTRiZGMtYWRmYy02OTZkYTQxNDBiZTgifQ==.fb55x5LYvPAW4wHlfzIFJ8PjUXUuiSnZdsCLSO5efu235SeeXF1Z1gv1c0cX3tqBko7bDZv3kNl0d32GyHAuggflinN45-MszERXmKdOjX50MZDLSF7F_N-v-B2woYFFx7DxS3HU3bhfVLtbOEWQBwlcobY2NOSN8q2v6AoY9cfTju7k3wyr0Hk6emuHMtzplQvyXNaeeWSQuWiUUeScSYY9F1yJhR5h2KOmorqQAA3AAoglrx2P_R17o0VgBSw4tFHHwvJ9PMfenh_7ygD1K9-dFd7xOW7zDhnseSkx49dAUhSPRlVYs7rl55_8c9ghavzoX7lhy9P1VfP16J-c9A==');
+    this.headers = headers.set('apikey', 'eyJ4NXQiOiJOak16WWpreVlUZGlZVGM0TUdSalpEaGtaV1psWWpjME5UTXhORFV4TlRZM1ptRTRZV1JrWWc9PSIsImtpZCI6ImdhdGV3YXlfY2VydGlmaWNhdGVfYWxpYXMiLCJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJzdWIiOiJhbmRyZXcucG95bnR6QG1ldG9mZmljZS5nb3YudWtAY2FyYm9uLnN1cGVyIiwiYXBwbGljYXRpb24iOnsib3duZXIiOiJhbmRyZXcucG95bnR6QG1ldG9mZmljZS5nb3YudWsiLCJ0aWVyUXVvdGFUeXBlIjpudWxsLCJ0aWVyIjoiVW5saW1pdGVkIiwibmFtZSI6InNpdGVfc3BlY2lmaWMtZDllNGQwZDktNTQ2YS00YTg5LWFmOGQtZWJiMzU3OTRmZmFiIiwiaWQiOjYxMzIsInV1aWQiOiJiMGQ0MDAwOC0wNzFkLTRjZmUtYWVmNS05NzE4ZjdlZDBlNjcifSwiaXNzIjoiaHR0cHM6XC9cL2FwaS1tYW5hZ2VyLmFwaS1tYW5hZ2VtZW50Lm1ldG9mZmljZS5jbG91ZDo0NDNcL29hdXRoMlwvdG9rZW4iLCJ0aWVySW5mbyI6eyJ3ZGhfc2l0ZV9zcGVjaWZpY19mcmVlIjp7InRpZXJRdW90YVR5cGUiOiJyZXF1ZXN0Q291bnQiLCJncmFwaFFMTWF4Q29tcGxleGl0eSI6MCwiZ3JhcGhRTE1heERlcHRoIjowLCJzdG9wT25RdW90YVJlYWNoIjp0cnVlLCJzcGlrZUFycmVzdExpbWl0IjowLCJzcGlrZUFycmVzdFVuaXQiOiJzZWMifX0sImtleXR5cGUiOiJQUk9EVUNUSU9OIiwic3Vic2NyaWJlZEFQSXMiOlt7InN1YnNjcmliZXJUZW5hbnREb21haW4iOiJjYXJib24uc3VwZXIiLCJuYW1lIjoiU2l0ZVNwZWNpZmljRm9yZWNhc3QiLCJjb250ZXh0IjoiXC9zaXRlc3BlY2lmaWNcL3YwIiwicHVibGlzaGVyIjoiSmFndWFyX0NJIiwidmVyc2lvbiI6InYwIiwic3Vic2NyaXB0aW9uVGllciI6IndkaF9zaXRlX3NwZWNpZmljX2ZyZWUifV0sInRva2VuX3R5cGUiOiJhcGlLZXkiLCJpYXQiOjE3MjU0NTU2NzEsImp0aSI6ImIwZTY5YmIzLTk0MmYtNGNjOC1hZWMzLTkyNWY5NmIyNmIwZiJ9.NvmvnqIcqN4DOj0xODz6ShDHNbH-cXyNFOnm0bdicw_-Hd2GwLAnNWjhsx1ij31AY1OFwRfkU-FtTaXRKFD2aDAWQMAXdIt58qzsDnDn2OqGKV4UhWajUMU3wYDTcAgiyD-miE19SjYVYtYcUSMlDn07o8ObVGb29YGpNa-zjPgcav8M8te7l1yDz8U1ZwEnk-8aCd4aG434ergj25v5jLV4yDXJqKPgJytMzbN0mTGWYs0J1p3UIVXfFUCQb3OFvz2AvfkxMMHNqAYvoVCKjXCqDlsC5GYTJA9zK03YSaakoeEhKXSoyU_pjLZR_Q3_TwcTPUshl2M7Jgsa5ado8g==');
   }
 
   
-  getData = (_lat, _lon) => {
-    
-    //this.httpClient.get(`https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly?latitude=${_lat}&longitude=${_lon}`, {headers:this.headers}).subscribe((hourly_data:any) => {
-      this.httpClient.get(`./hourlyData.json`, {headers:this.headers}).subscribe((hourly_data:any) => {
+  async getData(_lat, _lon) {
+    await this.getHourlyData(_lat, _lon);
+    await this.getDailyData(_lat, _lon);
+  }
+
+
+  async getHourlyData(_lat, _lon) {
+    //this.httpClient.get(`https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/hourly?latitude=${_lat}&longitude=${_lon}`, {headers:this.headers}).subscribe({next:(hourly_data:any) => {
+  
+    this.httpClient.get(`./hourlyData.json`, {headers:this.headers}).subscribe({next: (hourly_data:any) => {
       console.log(_lat, _lon);
-      //console.log(hourly_data);
-      this.hourlyData[0] = hourly_data.features[0].properties.timeSeries[2];
-      console.log(this.hourlyData);
-    })    
+    //  console.log(hourly_data);
+      let hourlyData = hourly_data.features[0].properties.timeSeries[2];
+      
+      console.log(hourlyData);
+      },
     
-    //this.httpClient.get(`https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/daily?latitude=${_lat}&longitude=${_lon}`, {headers:this.headers}).subscribe((daily_data:any) => {
-      this.httpClient.get(`./hourlyData.json`, {headers:this.headers}).subscribe((daily_data:any) => {
+    error: err => console.error('Observable emitted an error: ' + err),
+    complete: () => console.log('Observable emitted the complete notification')
+    })
+    return this.hourlyData;
+  }
+
+  async getDailyData(_lat, _lon) { 
+    //this.httpClient.get(`https://data.hub.api.metoffice.gov.uk/sitespecific/v0/point/daily?latitude=${_lat}&longitude=${_lon}`, {headers:this.headers}).subscribe({next:(daily_data:any) => {
+ 
+    this.httpClient.get(`./hourlyData.json`, {headers:this.headers}).subscribe({next: (daily_data:any) => {
       console.log(_lat, _lon);
       daily_data.features[0].properties.timeSeries.shift();
-      this.dailyData[0] = daily_data;
-      //this.dailyData[0] = this.dailyData.shift();
-      console.log(this.dailyData);
-          
+      let dailyData = daily_data;
+      console.log(dailyData);
+      },
+    error: err => console.error('Observable emitted an error: ' + err),
+    complete: () => console.log('Observable emitted the complete notification')
     })
-    this.allData = [this.hourlyData, this.dailyData];
-    this.location_data.push(this.allData);
-
+    return this.dailyData;
   }
-
-
-  
 
   /* getLocation (placeName) {
     console.log('get location data');
@@ -79,15 +88,9 @@ export class AppComponent implements OnInit{
   ngOnInit() {
     for(var i = 0;i<this.place_data.length;i++) {
       this.getData(this.place_data[i].lat, this.place_data[i].lon);
-      
-      //let LocationHourlyData[i] = this.hourlyData;
-      //let LocationDailyData[i] = this.dailyData;
-      //console.log(LocationHourlyData[i], LocationDailyData[i])
-
 
     }
-    console.log(this.location_data);
-//this is a comment for testing commit
+  //  console.log(this.location_data);
 
 /*
     clearSearchInput () {

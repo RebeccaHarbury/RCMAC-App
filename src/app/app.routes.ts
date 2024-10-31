9a8ef762d997046a3137973a0f7d8f57981fba9d
Rebecca Harbury <rebecca.harbury@metoffice.gov.uk>
2024-10-31 14:47:59 +0000

    Added state management effects

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,13 @@
 import { Route } from '@angular/router';
 import { DetailDisplayComponent } from './detail-display/detail-display.component';
 import { WeatherDisplayComponent } from './weather-display/weather-display.component';
+import { provideEffects } from '@ngrx/effects';
+import { FavouriteEffects } from './state/favourite/favourite.effects';
 
 export const AppRoutes: Route[] = [
   {
     path: '',
+    providers: [provideEffects(FavouriteEffects)],
     title: 'Home',
     component: WeatherDisplayComponent
   },

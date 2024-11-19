682e915cbabce2865f5c2ef5af4df3f33c6c674c
Rebecca Harbury <rebecca.harbury@metoffice.gov.uk>
2024-11-19 18:09:37 +0000

    Preferred time loaded by default with option to view current weather

--- src/app/weather-display/weather-display.component.ts
+++ src/app/weather-display/weather-display.component.ts
@@ -8,6 +8,7 @@ import { selectFavourite } from '../state/favourite/favourite.selectors';
 import { selectTime } from '../state/time/time.selectors';
 import { AppState } from '../state/app.state';
 import { addFavourite, deleteFavourite, loadFavourite, routeFavourite } from '../state/favourite/favourite.actions';
+import { loadTime } from '../state/time/time.actions';
 
 @Component({
   selector: 'weather-display-component',
@@ -24,29 +25,31 @@ import { addFavourite, deleteFavourite, loadFavourite, routeFavourite } from '..
 export class WeatherDisplayComponent implements OnInit {
   dateNow = DateTime.local().toISO();
   hourNow = Number(DateTime.local().toFormat("HH"));
-  prefTime$ = Number(17);
-  notCurrent = true;
+  prefTime$ = 17;
   current = false;
-
   locationData: any = [];
   fav_img = '';
   reroute = false;
   home = false;
-  
 
   service = inject(AppService);
 
   fav_location$: String = new String;
 
   constructor(private store: Store<AppState>) {
-    this.service.rerouteValue.subscribe((value) => this.reroute = value.valueOf())
-    this.service.homeValue.subscribe((value) => this.home = value.valueOf())
+    this.service.rerouteValue.subscribe((value) => this.reroute = value.valueOf());
+    this.service.homeValue.subscribe((value) => this.home = value.valueOf());
     if (this.reroute === true && this.home === false) {
-      this.store.dispatch(routeFavourite())
+      this.store.dispatch(routeFavourite());
     }
-    this.store.dispatch(loadFavourite())
+    this.store.dispatch(loadFavourite());
     this.store.select(selectFavourite).subscribe(favourite => {
-      this.fav_location$ = favourite
+      this.fav_location$ = favourite;
+    })
+
+    this.store.dispatch(loadTime())
+    this.store.select(selectTime).subscribe(time => {
+      this.prefTime$ = time;
     })
   }
 
@@ -55,10 +58,10 @@ export class WeatherDisplayComponent implements OnInit {
     event.preventDefault()
 
     if (this.fav_location$ !== location) {
-      this.store.dispatch(addFavourite({ location }))
+      this.store.dispatch(addFavourite({ location }));
     }
     else {
-      this.store.dispatch(deleteFavourite({ location }))
+      this.store.dispatch(deleteFavourite({ location }));
     }
   }
 
@@ -75,33 +78,40 @@ export class WeatherDisplayComponent implements OnInit {
 
   weatherIndex(current: boolean) {
     if (current === false) {
-      return this.findIndex()
+      return this.findIndex();
     }
     else {
-      return 1
+      return 1;
     }
   }
 
   findIndex() {
     if (this.hourNow < this.prefTime$) {
-      return this.locationData[0].hourlyData[0].findIndex((hour: any) => hour.time.includes(this.prefTime$+":00"))
-      
+      return this.locationData[0].hourlyData[0].findIndex((hour: any) => hour.time.includes(this.prefTime$ + ":00"));
     }
     else {
       return 1;
-    }    
+    }
   }
 
   currentWeather() {
-    this.current =! this.current;
+    if (this.hourNow < this.prefTime$) {
+      this.current = !this.current;
+    }
+    else if (this.hourNow > this.prefTime$) {
+      alert('Cannot load preferred time for today as it has passed. \n For a more detailed view please select a location.');
+    }
   }
 
   buttonText() {
     if (this.current === true) {
-      return 'Display weather for preferred time'
+      return 'Display weather for preferred time';
+    }
+    else if (this.hourNow > this.prefTime$) {
+      return 'Displaying current weather';
     }
     else {
-      return 'Display current weather'
+      return 'Display current weather';
     }
   }
 
@@ -110,7 +120,7 @@ export class WeatherDisplayComponent implements OnInit {
     const precip = data.probOfPrecipitation;
     const vis = data.visibility;
     return this.service.conditionHighlight(wind, precip, vis);
-  }  
+  }
 
   ngOnInit() {
     this.locationData.push(this.service.getData('Okehampton'));

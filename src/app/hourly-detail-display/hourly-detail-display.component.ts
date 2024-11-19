bf611c36e5ec37abcd5da6886265d333a0d776bc
Rebecca Harbury <rebecca.harbury@metoffice.gov.uk>
2024-11-19 18:10:15 +0000

    Hourly display scrolls to preferred time on loading

--- src/app/hourly-detail-display/hourly-detail-display.component.ts
+++ src/app/hourly-detail-display/hourly-detail-display.component.ts
@@ -2,6 +2,10 @@ import { Component, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { AppService } from '../app.service';
+import { Store } from '@ngrx/store';
+import { AppState } from '../state/app.state';
+import { loadTime } from '../state/time/time.actions';
+import { selectTime } from '../state/time/time.selectors';
 
 @Component({
   selector: 'hourly-detail-display',
@@ -12,20 +16,34 @@ import { AppService } from '../app.service';
   ],
   styleUrl: './hourly-detail-display.component.scss'
 })
+
 export class HourlyDetailComponent implements OnInit {
   specificLocationData: any = [];
+  prefTime$ = 17;
 
   service = inject(AppService);
 
   constructor(
+    private store: Store<AppState>,
     private route: ActivatedRoute,
-  ) { }
+  ) {
+    this.store.dispatch(loadTime());
+    this.store.select(selectTime).subscribe(time => {
+      this.prefTime$ = time;
+    })
+  }
 
   idealConditions(data: any) {
     const wind = data.windSpeed10m;
     const precip = data.probOfPrecipitation;
     const vis = data.visibility;
     return this.service.conditionHighlight(wind, precip, vis);
+  }
+
+  scrollFunction() {
+    const x = ((this.prefTime$ - 1) * 225);
+    const scrollElement = document.getElementsByClassName("scroll")[1];
+    scrollElement.scrollTo(x, 0);
   }
 
   ngOnInit(): void {
